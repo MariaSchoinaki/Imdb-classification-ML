b824b77a2c5bbc7577286e683b341acbfeed2c7a
Christos Stamoulos <147555306+ChristosStamoulos@users.noreply.github.com>
2024-02-12 01:25:03 +0200

    Update id_3.py

--- src/id_3.py
+++ src/id_3.py
@@ -1,112 +1,119 @@
+from statistics import mode
 import numpy as np
-from collections import Counter
+import math
 
 class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
-        self.feature = feature
-        self.threshold = threshold
-        self.left = left
-        self.right = right
-        self.value = value
+    def __init__(self, checking_feature=None, is_leaf=False, category=None):
+        self.checking_feature = checking_feature
+        self.left_child = None
+        self.right_child = None
+        self.is_leaf = is_leaf
+        self.category = category
         
-    def is_leaf_node(self):
-        return self.value is not None
 
 
-class DecisionTree:
-    def __init__(self, features, max_depth = 100, min_samples_split = 2):
-        self.features = features
-        self.max_depth = max_depth
-        self.min_samples_split = min_samples_split
+class ID3:
+    def __init__(self, features):
         self.tree = None
+        self.features = features
+    
+    def fit(self, x, y):
+        '''
+        creates the tree
+        '''
+        most_common = mode(y.flatten())
+        self.tree = self.create_tree(x, y, features=self.features, category=most_common)
         return self.tree
-
-    def fit(self, X, y):
-        self.root = self.create_tree(X, y)
-        return self.root
-    def create_tree(self, X, y, depth=0):
-        n_samples, n_feats = X.shape
-        n_labels = len(np.unique(y))
-
-        # check the stopping criteria
-        if (depth>=self.max_depth or n_labels==1 or n_samples<self.min_samples_split):
-            leaf_value = self._most_common_label(y)
-            return Node(value=leaf_value)
-
-        # find the best split
-        best_feature, best_thresh = self._best_split(X, y, self.features)
-
-        # create child nodes
-        left_idxs, right_idxs = self._split(X[:, best_feature], best_thresh)
-        left = self.create_tree(X[left_idxs, :], y[left_idxs], depth+1)
-        right = self.create_tree(X[right_idxs, :], y[right_idxs], depth+1)
-        return Node(best_feature, best_thresh, left, right)
-
-
-    def _best_split(self, X, y, features):
-        best_gain = -1
-        split_idx, split_threshold = None, None
-
-        for feat_idx in features:
-            X_column = X[:, feat_idx]
-            thresholds = np.unique(X_column)
-
-            for thr in thresholds:
-                # calculate the information gain
-                gain = self._information_gain(y, X_column, thr)
-
-                if gain > best_gain:
-                    best_gain = gain
-                    split_idx = feat_idx
-                    split_threshold = thr
-
-        return split_idx, split_threshold
-
-
-    def _information_gain(self, y, X_column, threshold):
-        # parent entropy
-        parent_entropy = self._entropy(y)
-
-        # create children
-        left_idxs, right_idxs = self._split(X_column, threshold)
-
-        if len(left_idxs) == 0 or len(right_idxs) == 0:
-            return 0
+    
+    def create_tree(self, x_train, y_train, features, category):
         
-        # calculate the weighted avg. entropy of children
-        n = len(y)
-        n_l, n_r = len(left_idxs), len(right_idxs)
-        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
-        child_entropy = (n_l/n) * e_l + (n_r/n) * e_r
-
-        # calculate the IG
-        information_gain = parent_entropy - child_entropy
-        return information_gain
-
-    def _split(self, X_column, split_thresh):
-        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
-        right_idxs = np.argwhere(X_column > split_thresh).flatten()
-        return left_idxs, right_idxs
-
-    def _entropy(self, y):
-        hist = np.bincount(y)
-        ps = hist / len(y)
-        return -np.sum([p * np.log(p) for p in ps if p>0])
-
-
-    def _most_common_label(self, y):
-        counter = Counter(y)
-        value = counter.most_common(1)[0][0]
-        return value
-
-    def predict(self, X):
-        return np.array([self._traverse_tree(x, self.root) for x in X])
-
-    def _traverse_tree(self, x, node):
-        if node.is_leaf_node():
-            return node.value
-
-        if x[node.feature] <= node.threshold:
-            return self._traverse_tree(x, node.left)
-        return self._traverse_tree(x, node.right)
+        # check empty data
+        if len(x_train) == 0:
+            return Node(checking_feature=None, is_leaf=True, category=category)  # decision node
         
+        # check all examples belonging in one category
+        if np.all(y_train.flatten() == 0):
+            return Node(checking_feature=None, is_leaf=True, category=0)
+        elif np.all(y_train.flatten() == 1):
+            return Node(checking_feature=None, is_leaf=True, category=1)
+        
+        if len(features) == 0:
+            return Node(checking_feature=None, is_leaf=True, category=mode(y_train.flatten()))
+        
+        igs = list()
+        for feat_index in features.flatten():
+            igs.append(self.calculate_ig(y_train.flatten(), [example[feat_index] for example in x_train]))
+        
+        max_ig_idx = np.argmax(np.array(igs).flatten())
+        m = mode(y_train.flatten())  # most common category 
+
+        root = Node(checking_feature=max_ig_idx)
+
+        # data subset with X = 0
+        x_train_0 = x_train[x_train[:, max_ig_idx] == 0, :]
+        y_train_0 = y_train[x_train[:, max_ig_idx] == 0].flatten()
+
+        # data subset with X = 1
+        x_train_1 = x_train[x_train[:, max_ig_idx] == 1, :]
+        y_train_1 = y_train[x_train[:, max_ig_idx] == 1].flatten()
+
+        new_features_indices = np.delete(features.flatten(), max_ig_idx)  # remove current feature
+
+        root.left_child = self.create_tree(x_train=x_train_1, y_train=y_train_1, features=new_features_indices, 
+                                           category=m)  # go left for X = 1
+        
+        root.right_child = self.create_tree(x_train=x_train_0, y_train=y_train_0, features=new_features_indices,
+                                            category=m)  # go right for X = 0
+        
+        return root
+
+
+    @staticmethod
+    def calculate_ig(classes_vector, feature):
+        classes = set(classes_vector)
+
+        HC = 0
+        for c in classes:
+            PC = list(classes_vector).count(c) / len(classes_vector)  # P(C=c)
+            HC += - PC * math.log(PC, 2)  # H(C)
+            # print('Overall Entropy:', HC)  # entropy for C variable
+            
+        feature_values = set(feature)  # 0 or 1 in this example
+        HC_feature = 0
+        for value in feature_values:
+            # pf --> P(X=x)
+            pf = list(feature).count(value) / len(feature)  # count occurences of value 
+            indices = [i for i in range(len(feature)) if feature[i] == value]  # rows (examples) that have X=x
+
+            classes_of_feat = [classes_vector[i] for i in indices]  # category of examples listed in indices above
+            for c in classes:
+                # pcf --> P(C=c|X=x)
+                pcf = classes_of_feat.count(c) / len(classes_of_feat)  # given X=x, count C
+                if pcf != 0: 
+                    # - P(X=x) * P(C=c|X=x) * log2(P(C=c|X=x))
+                    temp_H = - pf * pcf * math.log(pcf, 2)
+                    # sum for all values of C (class) and X (values of specific feature)
+                    HC_feature += temp_H
+        
+        ig = HC - HC_feature
+        return ig    
+
+        
+
+    def predict(self, x):
+        predicted_classes = list()
+
+        for unlabeled in x:  # for every example 
+            tmp = self.tree  # begin at root
+            while not tmp.is_leaf:
+                if unlabeled.flatten()[tmp.checking_feature] == 1:
+                    tmp = tmp.left_child
+                else:
+                    tmp = tmp.right_child
+            
+            predicted_classes.append(tmp.category)
+        
+        return np.array(predicted_classes)
+
+
+            
